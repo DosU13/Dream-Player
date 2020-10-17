3cec8f994120040daaa3cdc21777680bc4407c2b
DOSLAN <doslan130400@gmail.com>
2020-10-17 22:05:43 +0600

    add first releases

--- app/src/main/java/com/example/dream_player/MainActivity.kt
+++ app/src/main/java/com/example/dream_player/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.dream_player
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dream_player.adapters.TrackAdapter
 import com.example.dream_player.models.Track
+import java.util.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +24,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            start()
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        }
+    }
+
+    private fun start(){
         val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
 
         val projection = arrayOf<String>(
@@ -30,15 +48,33 @@ class MainActivity : AppCompatActivity() {
 
         val cursor = applicationContext.contentResolver.query(
             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-            projection, selection, null,null)
+            projection, selection, null, null
+        )
 
         val songs = mutableListOf<Track>()
         if (cursor != null) {
-            while(cursor.moveToNext()){
-                songs.add(Track(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3)))
+            while (cursor.moveToNext()) {
+                songs.add(
+                    Track(
+                        cursor.getString(0),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getString(3)
+                    )
+                )
             }
         }
         setUpEmailRecyclerView(songs)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(!grantResults.contains(PackageManager.PERMISSION_DENIED)){
+            start()
+        }
     }
 
     private fun setUpEmailRecyclerView(tracks: List<Track>) {
