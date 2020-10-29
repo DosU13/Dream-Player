16a6c10caf5b01e81bc4a414a6a186f889d5eb59
DOSLAN <doslan130400@gmail.com>
2020-10-29 19:35:35 +0600

    add fragments

--- app/src/main/java/com/example/dreamplayer/MainActivity.kt
+++ app/src/main/java/com/example/dreamplayer/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.dreamplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewPager();
+    }
+
+    private fun initViewPager() {
+        val viewPager = findViewById<ViewPager>(R.id.viewpager)
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.addFragments(SongsFragment(), "Songs")
+        viewPagerAdapter.addFragments(AlbumFragment(), "Albums")
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+
+    }
+
+    public class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        private var fragments = ArrayList<Fragment>()
+        private var titles = ArrayList<String>()
+
+        fun addFragments(fragment: Fragment, title: String){
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return  fragments.size
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titles[position]
+        }
     }
 }
