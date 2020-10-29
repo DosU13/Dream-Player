412724750c0de03f65973ddd35918dff8890d955
DOSLAN <doslan130400@gmail.com>
2020-10-29 22:54:30 +0600

    add a model and a permission for storage

--- app/src/main/java/com/example/dreamplayer/MainActivity.kt
+++ app/src/main/java/com/example/dreamplayer/MainActivity.kt
@@ -1,18 +1,53 @@
 package com.example.dreamplayer
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_CODE = 1
+    var musicFiles = ArrayList<MusicFiles>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        permission()
         initViewPager();
+    }
+
+    private fun permission(){
+        if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE)
+        }
+        else {
+            Toast.makeText(this, "Permission Granted!" , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted!" , Toast.LENGTH_SHORT).show()
+                musicFiles = getAllAudio(this)
+            }
+            else{
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE)
+            }
+        }
     }
 
     private fun initViewPager() {
@@ -26,7 +61,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    public class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         private var fragments = ArrayList<Fragment>()
         private var titles = ArrayList<String>()
 
@@ -46,5 +81,35 @@ class MainActivity : AppCompatActivity() {
         override fun getPageTitle(position: Int): CharSequence? {
             return titles[position]
         }
+    }
+
+    fun getAllAudio(context : Context) : ArrayList<MusicFiles> {
+        val tempAudioList = ArrayList<MusicFiles>()
+        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.ARTIST
+        )
+
+        val cursor = context.contentResolver.query(uri, projection, null, null, null)
+        if (cursor!=null){
+            while(cursor.moveToNext()){
+                val album = cursor.getString(0)
+                val title = cursor.getString(1)
+                val duration = cursor.getString(2)
+                val path = cursor.getString(3)
+                val artist = cursor.getString(4)
+
+                val musicFiles = MusicFiles(path, title, artist, album, duration)
+                Log.e("Path : $path", "Album : $album")
+                Toast.makeText(this, "Title : $album", Toast.LENGTH_SHORT).show()
+                tempAudioList.add(musicFiles)
+            }
+            cursor.close()
+        }
+        return tempAudioList
     }
 }
---
+++ app/src/main/java/com/example/dreamplayer/MusicFiles.kt
@@ -0,0 +1,10 @@
+package com.example.dreamplayer
+
+class MusicFiles (
+private val path : String,
+private val title : String,
+private val artist : String,
+private val album : String,
+private val duration : String
+)
+
